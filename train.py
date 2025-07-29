e3f199562977111437e3a1abb20cee347f2f5c4c
Nsengiyumva Wilberforce <nsengiyumvawilberforce@gmail.com>
2025-07-29 12:54:08 +0300

    tokenizer improvement

--- train.py
+++ train.py
@@ -11,6 +11,8 @@ from tokenizers import Tokenizer
 from tokenizers.models import WordLevel
 from tokenizers.trainers import WordLevelTrainer
 from tokenizers.pre_tokenizers import Whitespace
+from tokenizers.pre_tokenizers import BertPreTokenizer
+
 
 from torch.utils.tensorboard import SummaryWriter
 
@@ -95,7 +97,7 @@ def get_or_build_tokenizer(config, ds, lang):
     tokenizer_path = Path(config['tokenizer_file'].format(lang))
     if not Path.exists(tokenizer_path):
         tokenizer = Tokenizer(WordLevel(unk_token='[UNK]'))
-        tokenizer.pre_tokenizer = Whitespace()
+        tokenizer.pre_tokenizer = BertPreTokenizer()
         trainer = WordLevelTrainer(special_tokens = ["[UNK]", "[PAD]", "[SOS]", "[EOS]"], min_frequency=2)
         tokenizer.train_from_iterator(get_all_sentences(ds, lang), trainer=trainer)
         tokenizer.save(str(tokenizer_path))
